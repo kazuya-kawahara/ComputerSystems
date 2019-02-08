3231158dde88b175684187a51e6964fe1a32d229
kazuya <kawahara.k.ad@m.titech.ac.jp>
2019-02-08 17:27:06 +0900

    tim

---
+++ 4/1callee.s
@@ -0,0 +1,29 @@
+    .text
+main:
+    addi $sp, $sp, 4 # 
+    sw $ra, 0($sp)
+
+    li $t0, 0
+    li $t1, 0
+loop:
+    jal read1bit
+    
+    sll $t0, $t0, 1
+    add $t0, $t0, $v0
+
+    addi $t1, $t1, 1
+    
+    blt $t1, 8, loop
+
+    move $a0, $t0
+    li $v0, 1
+    syscall
+
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4 #
+    jr $ra
+
+read1bit:
+    li $v0, 5
+    syscall
+    jr $ra
---
+++ MIPSシミュレータ雛型/mips_student/src/adder.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <mips.h>
+
+void full_adder(Signal in1, Signal in2, Signal carry_in, Signal *out1, Signal *carry_out)
+{
+    /* Exercise 5-2 */
+}
+
+void rca(Word in1, Word in2, Signal carry_in, Word *out1, Signal *carry_out)
+{
+    /* Excercise 5-3 */
+}
+
+void test_full_adder()
+{
+    /* Exercise 5-2 */
+}
+
+void test_rca(int val1, int val2)
+{
+    /* Excercise 5-3 */
+}
+
